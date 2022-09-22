8ee1d98d947aaa6425fb56f38b8b416087c5c4f6
Chris <chris@hopfen.space>
2022-09-23 00:44:09 +0200

    Added FFI enums

--- porm/rorm_ffi.py
+++ porm/rorm_ffi.py
@@ -36,6 +36,75 @@ class ErrorTag(enum.IntEnum):
     COLUMN_INDEX_OUT_OF_BOUNDS = 9
 
 
+class ConditionTag(enum.IntEnum):
+    """
+    typedef int_t ConditionTag;
+    """
+
+    CONJUNCTION = 0
+    DISJUNCTION = 1
+    UNARY_CONDITION = 2
+    BINARY_CONDITION = 3
+    TERTIARY_CONDITION = 4
+    VALUE = 5
+
+
+class UnaryConditionTag(enum.IntEnum):
+    """
+    typedef int_t UnaryConditionTag;
+    """
+
+    IS_NULL = 0
+    IS_NOT_NULL = 1
+    EXISTS = 2
+    NOT_EXISTS = 3
+    NOT = 4
+
+
+class BinaryConditionTag(enum.IntEnum):
+    """
+    typedef int_t BinaryConditionTag;
+    """
+
+    EQUALS = 0
+    NOT_EQUALS = 1
+    GREATER = 2
+    GREATER_OR_EQUALS = 3
+    LESS = 4
+    LESS_OR_EQUALS = 5
+    LIKE = 6
+    NOT_LIKE = 7
+    REGEXP = 8
+    NOT_REGEXP = 9
+    IN = 10
+    NOT_IN = 11
+
+
+class TertiaryConditionTag(enum.IntEnum):
+    """
+    typedef int_t TertiaryConditionTag;
+    """
+
+    BETWEEN = 0
+    NOT_BETWEEN = 1
+
+
+class ValueTag(enum.IntEnum):
+    """
+    typedef int_t ValueTag;
+    """
+
+    IDENT = 0
+    STRING = 1
+    I64 = 2
+    I32 = 3
+    I16 = 4
+    BOOL = 5
+    F64 = 6
+    F32 = 7
+    NULL = 8
+
+
 class FFIString(ctypes.Structure):
     """
     typedef struct FFIString {
