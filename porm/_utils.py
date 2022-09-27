3da8ab7b1f5a0015d72c66ada23f216531661f90
Chris <chris@hopfen.space>
2022-09-28 01:48:12 +0200

    Added some more utilities

--- porm/_utils.py
+++ porm/_utils.py
@@ -1,3 +1,6 @@
+import ctypes
+import asyncio
+import contextlib
 from typing import Any, Generator
 
 
@@ -8,3 +11,17 @@ def _counter(start: int = 0) -> Generator[int, Any, Any]:
             raise RuntimeError(f"Exceeding int64_t range: {n}")
         yield n
         n += 1
+
+
+def is_null_ptr(ptr: ctypes.POINTER) -> bool:
+    try:
+        _ = ptr.contents
+        return False
+    except ValueError:
+        return True
+
+
+async def wait_event(event: asyncio.Event, timeout: float):
+    with contextlib.suppress(asyncio.TimeoutError):
+        await asyncio.wait_for(event.wait(), timeout)
+    return event.is_set()
